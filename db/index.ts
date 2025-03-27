44490ed64efe33fb9a49bb5012e60c2723b23220
Raju Kumar <59579007+therajusah@users.noreply.github.com>
2025-03-27 23:51:04 +0530

    fix sigleton

--- db/index.ts
+++ db/index.ts
@@ -8,11 +8,11 @@ declare global {
     let prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-
+//@ts-ignore
 const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
-
 if (process.env.NODE_ENV !== 'production') {
+    //@ts-ignore
     globalThis.prisma = prisma;
 }
